3015a6fd72c42ff41ddfb9e0b82c66db00e6aeb2
izzudin26 <izzudin.ar26@gmail.com>
2022-07-30 11:27:24 +0700

    add gap in grid

--- components/landing/testi.js
+++ components/landing/testi.js
@@ -6,9 +6,9 @@ const Testimonial = () => {
     <>
       <div className="container px-5 mx-auto w-full py-10 items-center justify-center text-center">
         <div className="font-semibold bg-clip-text text-transparent text-3xl bg-gradient-to-r from-green-500 to-blue-500">Testimonial</div>
-        <div className="w-full mt-3 grid grid-cols-1 lg:grid-cols-3 items-center justify-center">
+        <div className="w-full mt-3 grid grid-cols-1 lg:grid-cols-3 gap-5 items-center justify-center">
           {dummy.map((_) => (
-            <div className="w-96 h-48 rounded-lg p-10 shadow ring-1 ring-gray-200 hover:ring-2 duration-500 hover:ring-gray-400 mx-auto flex flex-col">
+            <div className="w-96 h-48 rounded-lg p-10 shadow ring-1 ring-gray-200 hover:ring-2 duration-500 hover:ring-blue-400 mx-auto flex flex-col">
                 <div className="flex flex-row items-center justify-start space-x-3">
                     <img src="/assets/ex_profil.jpg" className="w-14 h-14 rounded-full object-center object-cover"/>
                     <div className="flex flex-col text-left">
