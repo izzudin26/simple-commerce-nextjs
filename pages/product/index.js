a96b00e08c02e09dd51bba06f9f9b2d4af855ca1
izzudin26 <izzudin.ar26@gmail.com>
2022-08-02 22:43:59 +0700

    add key

--- pages/product/index.js
+++ pages/product/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Layout from "../../components/layout";
 import { useState } from "react";
 import Product from "../../components/product/product";
+import Link from "next/link";
 
 const Products = () => {
   const [items] = useState([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
@@ -16,7 +17,7 @@ const Products = () => {
             <div className="bg-clip-text text-transparent font-semibold text-xl bg-gradient-to-t from-blue-600 to-black">Produk Terbaru</div>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 w-full gap-5 mt-5">
               {items.map((item, i) => (
-                <Product name="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy" price={12000} />
+                <Product key={i} name="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy" price={12000} />
               ))}
             </div>
           </div>
