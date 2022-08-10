c979e2f7ca98781770682adb65664b423669c2cd
izzudin26 <izzudin.ar26@gmail.com>
2022-08-10 15:57:51 +0700

    add desktop navbar

--- components/seller/navbar.js
+++ components/seller/navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useState } from "react";
 
 const SellerNavbar = () => {
-  const [toggleNavbar, setToggleNavbar] = useState(true);
+  const [toggleNavbar, setToggleNavbar] = useState(false);
 
   const switchNav = () => {
     setToggleNavbar(!toggleNavbar);
@@ -10,7 +10,8 @@ const SellerNavbar = () => {
 
   return (
     <>
-      <div className="w-full flex-row space-x-5 p-5 items-center flex justify-between shadow">
+      {/* Navbar Mobile */}
+      <div className="w-full flex lg:hidden flex-row space-x-5 p-5 items-center justify-between shadow">
         <div className="text-2xl text-transparent bg-clip-text bg-gradient-to-t from-green-500 to-blue-500 font-semibold">Store Name</div>
         <button onClick={switchNav} className="p-1 py-2 rounded-md hover:text-blue-600 focus:outline-none">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -18,22 +19,37 @@ const SellerNavbar = () => {
           </svg>
         </button>
       </div>
-      <div className={`${toggleNavbar ? ' w-64  p-5 ' : 'w-0 p-0'} overflow-hidden flex flex-col transform transition-width duration-500 fixed right-0 top-0 h-screen bg-blue-600 space-y-5 z-30 rounded-l-sm items-start`}>
+      <div className={`${toggleNavbar ? " w-64  p-5 " : "w-0 p-0"} overflow-hidden flex lg:hidden flex-col transform transition-width duration-500 fixed right-0 top-0 h-screen bg-blue-600 space-y-5 z-30 rounded-l-sm items-start`}>
         <button onClick={switchNav}>
           <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
           </svg>
         </button>
         <Link href="/seller">
-            <button className="flex w-full text-white text-lg">Dashboard</button>
+          <button className="flex w-full text-white text-lg">Dashboard</button>
         </Link>
         <Link href="/seller/product">
-            <button className="flex w-full text-white text-lg">Produk</button>
+          <button className="flex w-full text-white text-lg">Produk</button>
         </Link>
         <Link href="/seller/profil">
-            <button className="flex w-full text-white text-lg">Profil</button>
+          <button className="flex w-full text-white text-lg">Profil</button>
         </Link>
       </div>
+      {/* End Navbar Mobile */}
+      {/* Navbar Desktop */}
+      <div className="w-64 hidden lg:flex flex-col bg-blue-600 h-screen fixed top-0 left-0 p-5 space-y-5">
+        <div className="text-3xl font-semibold mx-auto text-white">Store Name</div>
+        <Link href="/seller">
+          <button className="flex w-full text-white text-lg">Dashboard</button>
+        </Link>
+        <Link href="/seller/product">
+          <button className="flex w-full text-white text-lg">Produk</button>
+        </Link>
+        <Link href="/seller/profil">
+          <button className="flex w-full text-white text-lg">Profil</button>
+        </Link>
+      </div>
+      {/* End Navbar Desktop */}
     </>
   );
 };
